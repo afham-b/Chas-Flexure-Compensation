5b74f200dc2824fc21dcec58b392e30f2e0edfbf
afham-b <afhambashir@yahoo.com>
2024-06-28 17:10:41 -0400

    pywin auto launch time

--- mg_track.py
+++ mg_track.py
@@ -9,15 +9,16 @@ import os
 
 idguide      = win32api.RegisterWindowMessage("MG_RemoteGuide")
 
-# Start MetaGuide
+# Start MetaGuide via the plain .exe
 # app = Application().start(r"C:\Program Files (x86)\MetaGuide\MetaGuide.exe")
-time.sleep(10)  # Wait for MetaGuide to open
+#time.sleep(10)  # Wait for MetaGuide to open
 
 # put in your metaguide setup file if its saved, ours is test1
 scope_setup_path = r'C:\Users\afham\Documents\MetaGuide\test1.mg'
 os.startfile(scope_setup_path)
-time.sleep(20)
+time.sleep(5)
 
-# start guide
-win32api.PostMessage(win32con.HWND_BROADCAST, idguide, 0, 0)
+# start guide (uncomment if needed)
+#win32api.PostMessage(win32con.HWND_BROADCAST, idguide, 0, 0)
+
 
